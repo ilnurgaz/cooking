d9448deef9d5c96370e885df0fe14c30899e9aeb
Голубев Никита <nikitagolybev250505@gmail.com>
2024-04-16 16:08:15 +0400

    contact1

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -20,24 +20,24 @@
                     <form class="obr-form" action="/contact/submit" method="post">
                         <div class="form-group">
                             <label for="name">Введите имя</label>
-                            <input type="text" name="name" placeholder="Введите имя" id="name" class="form-control">
+                            <input class="input-obr" type="text" name="name" placeholder="Введите имя" id="name" class="form-control">
                         </div>
     
                         <div class="form-group">
                             <label for="email">Email</label>
-                            <input type="text" name="email" placeholder="Введите email" id="email" class="form-control">
+                            <input class="input-obr" type="text" name="email" placeholder="Введите email" id="email" class="form-control">
                         </div>
     
                         <div class="form-group">
                             <label for="subject">Тема сообщения</label>
-                            <input type="text" name="subject" placeholder="Тема сообщения" id="subject" class="form-control">
+                            <input class="input-obr" type="text" name="subject" placeholder="Тема сообщения" id="subject" class="form-control">
                         </div>
     
                         <div class="form-group">
                             <label for="message">Сообщение</label>
                             <textarea name="message" id="message" class="form-control" placeholder="Введите сообщение"></textarea>
                         </div>
-                         <button type="submit" class="btn btn-success">Отправить</button>
+                         <button type="submit" class="btn-success">Отправить</button>
     
                     </form>
                 </div>
