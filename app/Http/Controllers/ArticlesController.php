d977c4b817d5cdab5a9cab5c9a738a43917fc027
Голубев Никита <nikitagolybev250505@gmail.com>
2024-04-17 22:30:04 +0400

    gg

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -3,10 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\ArticlesRequest;
+use App\Models\Articles;
 
 class ArticlesController extends Controller
 {
     public function submit(Request $req) {
-     
+     $articles = new Articles();
+     $articles->theme = $req->input('theme');
+     $articles->image = $req->input('image');
+     $articles->content = $req->input('content');
+
+     $articles->save();
+
+     return redirect()->route('admin-articles');
     }
 }
