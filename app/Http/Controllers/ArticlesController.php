d23d0beba124e337df498a5f9dd6a293d4c53f1d
Голубев Никита <nikitagolybev250505@gmail.com>
2024-04-18 11:28:43 +0400

    img

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -11,8 +11,25 @@ class ArticlesController extends Controller
     public function submit(Request $req) {
      $articles = new Articles();
      $articles->theme = $req->input('theme');
-     $articles->image = $req->input('image');
      $articles->content = $req->input('content');
+
+     $image = $req->file('image');
+     if($image) {
+         $imageName = $image->getClientOriginalName(); 
+         $articles->image = $imageName; 
+         $tmpPath = $image->getPathname();
+         $path = public_path('./assets/image/articles');
+     }
+     else {
+         $articles->image = 'image-placeholder.png'; 
+     }
+
+     if($image) {
+        move_uploaded_file($tmpPath, $path . '/' . $imageName);
+        }
+
+
+
 
      $articles->save();
 
