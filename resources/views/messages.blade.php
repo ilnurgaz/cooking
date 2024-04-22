8d166722333ba974f40bfd6203e42b6ff7d6f3a4
ilnurgaz <131668262+ilnurgaz@users.noreply.github.com>
2024-04-22 18:14:18 +0400

    fix

--- resources/views/messages.blade.php
+++ resources/views/messages.blade.php
@@ -1,14 +1,16 @@
 <x-header-admin/>
-<div style="  margin: 0 auto;">
-    <h1 style="text-align: center;">Все сообщения</h1>
-</div>
-@foreach($data as $el)
-<div class="alert-info">
-    <h3 class="heading3" >{{ $el->name}}</h3>
-    <p>{{ $el->email}}</p>
-    <p>{{ $el->subject}}</p>
-    <p>{{ $el->message}}</p>
-    <p> <small class="smals"> {{ $el->created_at}}</small> </p>
-</div>
-@endforeach
+    <div class="bloks_wrapper">
+        <div class="block_container">
+            <h2 class="admin_title">Все сообщения</h2>
+            @foreach($data as $el)
+            <div class="alert-info">
+                <h3 class="heading3" >{{ $el->name}}</h3>
+                <p>{{ $el->email}}</p>
+                <p>{{ $el->subject}}</p>
+                <p>{{ $el->message}}</p>
+                <p> <small class="smals"> {{ $el->created_at}}</small> </p>
+            </div>
+            @endforeach
+        </div>
+    </div>
 <x-footer-admin/>
