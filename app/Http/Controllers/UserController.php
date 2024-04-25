01d543613ddf989976adc591d5ae8513faee966d
ilnurgaz <131668262+ilnurgaz@users.noreply.github.com>
2024-04-25 14:31:22 +0400

    fix

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -30,8 +30,8 @@ class UserController extends Controller
         }
         else {
             $count_recipes = Recipes::count();
-            $categories = categories::orderBy('created_at', 'desc')->get();
-            $articles = Articles::orderBy('created_at', 'asc')->take(4)->get();
+            $categories = categories::orderBy('created_at', 'asc')->get();
+            $articles = Articles::orderBy('created_at', 'desc')->take(4)->get();
             $recipes = Recipes::orderBy('created_at', 'desc')->take(20)->get();
             return view('recipes', ['categories' => $categories, 'count' => $count_recipes, 'recipes' => $recipes, 'page' => 0, 'articles' => $articles, 'cat_pag' => false, 'cat_active' => false]);
         }
@@ -40,7 +40,7 @@ class UserController extends Controller
     public function recipesPagination($page) {
         $offset = $page * 20;
         $count_recipes = Recipes::count();
-        $articles = Articles::orderBy('created_at', 'desc')->take(4)->get();
+        $articles = Articles::orderBy('created_at', 'asc')->take(4)->get();
         $categories = categories::orderBy('created_at', 'asc')->get();
         $recipes = Recipes::orderBy('created_at', 'desc')->offset($offset)->take(20)->get();
         return view('recipes', ['categories' => $categories, 'count' => $count_recipes, 'recipes' => $recipes, 'page' => $page, 'articles' => $articles, 'cat_pag' => false, 'cat_active' => false]);
